4e68d8a1d762b6b985a5fc58caf79141db3ce742
WIDANET <willianlps99@outlook.com>
2023-07-31 16:59:42 -0300

    Atividade Do While 31/07

--- src/Problema_DoWhile.java
+++ src/Problema_DoWhile.java
@@ -1,9 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
 
-public class New_Problem {
+public class Problema_DoWhile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		// VARIÁVEIS
+		double temp, F;
+		char resp;
+		
+		// ENTRADA E PROCESSAMENTO DE DADOS
+		do {
+		System.out.print("Digite o valor em Celsius: ");
+		temp = sc.nextDouble();
+		F = 9.0 * temp / 5.0 + 32.0;
+		
+		System.out.printf("A temperatura em Fahrenheit é: %.1f%n", F);
+		
+		System.out.println("Deseja continuar? (S/N) ");
+		resp = sc.next().charAt(0);
+
+		} while ( resp != 'n');
+		
+		sc.close();
+
 
 	}
-
 }
